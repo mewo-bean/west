8ef687912c393d0278127de4d527cf2b6aec62bc
mewo-bean <annybezrukova@gmail.com>
2026-03-17 18:31:38 +0500

    1-3

--- src/index.js
+++ src/index.js
@@ -27,30 +27,52 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
-
+class Creature extends Card {
+    getDescriptions() {
+        getCreatureDescription()
+        return 
+    }
+}
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Creature {
+    constructor() {
+        super();
+        this.name = "Мирная утка";
+        this.currentPower = 2;
+    }
+
+    quacks() {
+        console.log('quack')
+    }
+
+    swims() {
+        console.log('float: both;')
+    }
+
 }
 
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Creature {
+    constructor() {
+        super();
+        this.name = "Пес-бандит";
+        this.currentPower = 3;
+    }
 }
 
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
 
 
